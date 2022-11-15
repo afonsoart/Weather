106e0bcde2ca8bf16416797afa5d6de240bb41d2
Afonso Ramon de Araujo Teles <afonso1907@hotmail.com>
2022-11-15 19:56:00 -0300

    background img fix

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ let weather = {
       const { speed } = data.wind;
       document.querySelector(".city").innerText = "Weather in " + name;
       document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
+        "https://openweathermap.org/img/wn/" + icon + ".png" ;
       document.querySelector(".description").innerText = description;
       document.querySelector(".temp").innerText = temp + "°C";
       document.querySelector(".humidity").innerText =
